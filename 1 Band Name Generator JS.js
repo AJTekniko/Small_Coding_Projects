108a06266359cb32ec153b480e5cdc497f835ccd
AJTekniko <ajt3chmail@gmail.com>
2025-11-19 23:14:22 -0700

    Add files via upload

---
+++ 1 Band Name Generator JS.js
@@ -0,0 +1,9 @@
+//ask for city user grew up in
+console.log("Welcome to the Band Name Generator.")
+city = prompt("What's the name of the city you grew up in?\n")
+
+//ask for user's name
+pet = prompt("What's your pet's name?\n")
+
+//combine input
+console.log(`Your band name could be: ${city} ${pet}`)
